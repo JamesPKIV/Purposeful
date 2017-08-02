a1ac71c264b602cfb8e7fc3a1466ccf0b0b5c4ec
digonsan <digonsan@hotmail.com>
2017-08-01 22:21:44 -0500

    fixed green, nav bar detail, and made text be more centered

--- purposeful_app/src/components/BelieveContent/BelieveContent.js
+++ purposeful_app/src/components/BelieveContent/BelieveContent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import './BelieveContent.css';
 
 class BelieveContent extends Component {
 
@@ -10,30 +10,38 @@ class BelieveContent extends Component {
 				<h1>What We Believe</h1>
 				<section>
 					<h3>Our Motivation</h3>
-					<p>
-					 	Although many people are eager to make a difference and have great ideas about how to do that, 
-					 	few of them are actually able to find opportunities to bring to life these ideas that they truly
-					  	care about. The lack of diversity in many different disciplines creates a feeling of otherness
-					   	that makes it harder for people who do not fit the stereotype to get involved with the disciplines
-					    they are interested in. Therefore, many individuals find themselves doing work that they don't enjoy 
-					    in order to reach goals that they don't care about, and lacking the necessary knowledge and skills
-					    to bring their own ideas to life. By providing a free platform for mentorship, interdisciplinary 
-					    projects, and great idea sharing, we hope to help individuals from all different 
-					    backgrounds find a purpose in the projects that they choose to spend their time doing.   
+					<p id="content">
+					 	Although many people are eager to make a difference in the world and
+						have great ideas on how to do that, only a few of them are actually
+						able to find opportunities that bring those ideas to life. We have
+						noticed that the lack of diversity in many disciplines creates a
+						feeling of otherness that makes it harder for people who do not "fit
+						the stereotype" to get involved in activities that they care about.
+						Due to this otherness, many individuals find themselves doing work
+						that they do not enjoy, and lacking the necessary knowledge and
+						skills to develop their own ideas. By fomenting diversity and
+						providing a free platform for mentorship, interdisciplinary projects,
+						and great idea sharing, we hope to help individuals from many
+						different backgrounds to find a purpose in the projects that they
+						choose to work for.
 					</p>
 				</section>
 
 				<section>
 					<h3>Our Vision for the Future</h3>
-					<p>
-						We envision a world where all people have an opportunity to make a real, meaningful impact towards
-						great ideas and causes they are passionate about, regardless of their backgrounds and identities. 
-						Furthermore, we believe people should be encouraged to pursue purposeful ideas that may encompass 
-						multiple disciplines, because oftentimes real world projects don't fit neatly into any single 
-						discipline. We see a world where all people can blaze their own trail and create their own purpose 
-						in life, no matter who they are, or where they come from, only where they want to go. Ultimately, 
-						we want to see each person become excited about the work they spend their lives doing and be proud 
-						of what they create. To us, a life with purpose is a life worth living.	
+					<p id="content">
+						We envision a world where all people have the opportunity to make a
+						real, meaningful impact towards causes they are passionate about,
+						and to work on innovative ideas, regardless of their backgrounds and
+						identities. Real world projects oftentimes do not fit neatly in a
+						single discipline, therefore, we believe that people should be
+						encouraged to pursue purposeful ideas that may encompass multiple
+						disciplines. In the future, we see a world where all people can blaze
+						their own trail and create their own purpose in life, no matter who
+						they are, or where they come from, only where they want to go.
+						Ultimately, we want to see each person become excited about the work
+						they spend their lives doing, and become proud of waht they create.
+						We believe that a life with a purpose is a life worth living.
 					</p>
 				</section>
 			</article>
