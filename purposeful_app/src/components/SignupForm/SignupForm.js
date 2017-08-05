d7cda52c2e2aec6579b5da5e7e38e7b5debd2a33
JamesPKIV <james@bePurposeful.co>
2017-08-05 09:24:19 -0700

    signup form input validation fix

--- purposeful_app/src/components/SignupForm/SignupForm.js
+++ purposeful_app/src/components/SignupForm/SignupForm.js
@@ -5,10 +5,21 @@ class SignupForm extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
     this.props.handleChange(e);
+  }
+
+  handleSubmit(event) {
+
+    //prevent form reset
+    event.preventDefault();
+    //if form is valid, send it
+    if (event.target.checkValidity() == true) {
+      this.props.onClick();
+    }
   }
 
   render() {
@@ -20,13 +31,13 @@ class SignupForm extends Component {
 
         <section>
         { this.props.isFormShowing ?
-            <form id="signupForm">
+            <form id="signupForm" onSubmit={this.handleSubmit}>
               <p>  Sign up for our mailing list to keep up to date with Purposeful. </p>
 
               <input type="text" name="first" placeholder="First Name" onChange={this.handleChange} autoFocus required />
               <input type="text" name="last" placeholder="Last Name" onChange={this.handleChange} required />
               <input type="email" name="email" placeholder="Email" onChange={this.handleChange} required />
-              <input type="button" value="Join our mailing list!" onClick={() => this.props.onClick()}/>
+              <input type="submit" value="Join our mailing list!" />
             </form>
           :
             <h4>Democratizing the web through the free sharing of knowledge and great ideas. </h4>
