5460afa91e402afecc6744884ca42c86c7e207f9
digonsan <digonsan@hotmail.com>
2017-08-10 19:42:53 -0500

    fixed mobile nav yay

--- purposeful_app/src/components/NavBar/NavBar.js
+++ purposeful_app/src/components/NavBar/NavBar.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
-import FaAlignJustify from 'react-icons/lib/fa/align-justify'
+import FaAlignJustify from 'react-icons/lib/fa/align-justify';
+import FaClose from 'react-icons/lib/fa/close';
 
 class NavBar extends Component {
 
@@ -14,7 +15,7 @@ class NavBar extends Component {
     this.hideNav = this.hideNav.bind(this);
     this.toggle = this.toggle.bind(this);
   }
-  
+
   componentWillMount() {
     if (this.props.containerWidth >= 700){
       this.showNav();
@@ -34,7 +35,7 @@ class NavBar extends Component {
         activeNav: false
       });
   }
-  
+
   toggle() {
         this.setState ({
           activeNav : !this.state.activeNav
@@ -61,10 +62,13 @@ class NavBar extends Component {
       <nav className="NavBar">
         <ul>
           <button className="toggleBtn open" onClick={this.hideNav} >
-          <li><NavLink to="/mailingList" className="hvr-sweep-to-top" >Subscribe</NavLink></li>
-          <li><NavLink to="/whatWeBelieve" className="hvr-sweep-to-top">Our Story </NavLink></li>
-          <li><NavLink to="/whatWeDo" className="hvr-sweep-to-top">Our Pillars </NavLink></li>
-          <li><NavLink to="/contact" className="hvr-sweep-to-top">About us </NavLink></li>
+            <li><NavLink to="/mailingList" className="hvr-sweep-to-top" >Subscribe</NavLink></li>
+            <li><NavLink to="/whatWeBelieve" className="hvr-sweep-to-top">Our Story </NavLink></li>
+            <li><NavLink to="/whatWeDo" className="hvr-sweep-to-top">Our Pillars </NavLink></li>
+            <li><NavLink to="/contact" className="hvr-sweep-to-top">About us </NavLink></li>
+          </button>
+          <button className="toggleBtn closed" onClick={this.toggle.bind(this)}>
+            <FaClose />
           </button>
         </ul>
       </nav>
@@ -75,14 +79,16 @@ class NavBar extends Component {
     /* desktop version */
     if (this.props.containerWidth >= 700){
     return this.navBarDesktop();
-    } 
+    }
     /* mobile version */
     else {
        if (this.state.activeNav){
           return this.navBarMobile();
         } else {
           return (
-            <button className="toggleBtn closed" onClick={this.toggle.bind(this)}> <FaAlignJustify /> </button>
+            <button className="toggleBtn closed" onClick={this.toggle.bind(this)}>
+              <FaAlignJustify />
+            </button>
         );
         }
   }
