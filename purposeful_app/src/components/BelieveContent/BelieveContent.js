e306ce477a7be05da2e83bc64267ee5d34a5c902
navidmx <navidmx@gmail.com>
2017-08-21 13:12:54 -0400

    Fixing language and pillars

--- purposeful_app/src/components/BelieveContent/BelieveContent.js
+++ purposeful_app/src/components/BelieveContent/BelieveContent.js
@@ -17,9 +17,9 @@ class BelieveContent extends Component {
 					<div  className="strongest">Every day millions of great <span id="ideas-color0">ideas</span>, <span id="ideas-color1">dreams</span>, and <span id="ideas-color2">aspirations</span> go unfilled...</div>
 						<br/>
 						<div id="believe-description">
-					    We the Purposeful team believe that when people have access to the right skills, resources, and network, those ideas can be heard. 
-						That our voice as a collective community is stronger when we are fueled by our passions and guided by those with experience and similar passions as us.
-					    We decided to build a platform for people to find or pursue their purpose in life. A social platform for connecting us to our purpose. 
+					    We at Purposeful believe that when people have access to the right skills, resources, and network, their ideas can be heard.
+						Our voice as a collective community is stronger when we are fueled by our passions and guided by those with experience and similar passions.
+					    We decided to build a platform for people to find and pursue their purpose in life. A social platform for connecting us to our purpose.
 						</div>
 						<br/>
 						<div className="strongest">
