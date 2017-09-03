b9a2b2a756e260a2d13e30bb1af61940a6ba6c86
digonsan <digonsan@hotmail.com>
2017-09-03 15:09:47 -0700

    added oscar's new article

--- purposeful_app/src/components/BelieveContent/BelieveContent.js
+++ purposeful_app/src/components/BelieveContent/BelieveContent.js
@@ -7,7 +7,7 @@ class BelieveContent extends Component {
 		return (
 			<section className="believe-container semi-transparent-box">
 				<h2 id="believe-title">Why build Purposeful?</h2>
-				
+
 				<div id="believe-content">
 					<div  className="strongest">Every day millions of great <span id="ideas-color0">ideas</span>, <span id="ideas-color1">dreams</span>, and <span id="ideas-color2">aspirations</span> go unfulfilled...
 				 	</div>
@@ -15,11 +15,11 @@ class BelieveContent extends Component {
 					<div id="believe-description">
 					    We believe that when people have access to the right skills, resources, and network, those great ideas can grow into great endeavors.
 						That our voice as individuals and as a collective community is stronger when we are fueled by our passions and guided by those with experience who want to help us succeed.
-					    We are building a platform for people to find, pursue or create their own purpose in life. 
+					    We are building a platform for people to find, pursue or create their own purpose in life.
 					</div>
 					<br/>
 					<div className="strongest">
-						Purposeful is a social platform for connecting us to our purpose -   
+						Purposeful is a social platform for connecting us to our purpose -
 						<br/> for being and becoming Purposeful as a community.
 					</div>
 					<br/>
@@ -37,6 +37,11 @@ class BelieveContent extends Component {
                             <a href="https://www.linkedin.com/pulse/compiled-success-why-some-people-find-quicker-than-others-oscar-parra?trk=mp-reader-card" target="_newtab">
                                 <li className="left">
                                     Compiled Success: Why some people find success quicker than others?
+                                </li>
+                            </a>
+														<a href="https://www.linkedin.com/pulse/calculated-risk-believing-your-co-founders-oscar-parra?trk=mp-reader-card" target="_newtab">
+                                <li className="left">
+                                    A calculated risk: Believing in your co-founders
                                 </li>
                             </a>
 						</ul>
