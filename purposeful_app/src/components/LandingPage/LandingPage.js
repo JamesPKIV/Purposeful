18c845235a8c75a59f740d896bce61ac89c10be7
ogparra <ogparra.15@gmail.com>
2017-09-15 17:38:17 -0700

    Made UI changes, removed bg-img for now

--- purposeful_app/src/components/LandingPage/LandingPage.js
+++ purposeful_app/src/components/LandingPage/LandingPage.js
@@ -3,60 +3,87 @@ import './LandingPage.css';
 import logo from './logo.png';
 import FaAngleDown from 'react-icons/lib/fa/angle-down';
 import FaAngleUp from 'react-icons/lib/fa/angle-up';
+import FaFacebook from 'react-icons/lib/fa/facebook-square';
+import FaLinkedin from 'react-icons/lib/fa/linkedin-square';
+import FaGoogle from 'react-icons/lib/fa/google-plus-square';
 
 class LandingPage extends Component {
 
-	constructor (props) {
+	constructor(props) {
 		super(props);
 		this.state = {
-			isLoggedIn : false,
-			inputInfo: false
+			isLoggedIn: false,
+			inputInfo: false,
+			nameSet: false
 		};
-  }
+	}
 
 	toggle = () => {
-        this.setState ({
-          inputInfo : !this.state.inputInfo
-        });
-  }
+		this.setState({
+			nameSet: !this.state.nameSet
+		});
+	}
 
-	becomePurposeful(){
-		if (this.state.inputInfo){
+	becomePurposeful = () => {
+		return (
+			<div>
+				<h1>Welcome to Purposeful</h1>
+			</div>
+		);
+	}
+
+	purposeful_Signup = () => {
+		if (!this.state.nameSet) {
 			return (
 				<div>
-					<h1>Become Prposeful</h1>
-					<input className="inputName" type="text" name="FirstName" value="Tell Us Your Name"/>
-					<a className="contGuest" href="dummy5"> Continue as Guest </a>
-					<button className="arrowButtonUp" onClick={this.toggle}> <FaAngleUp/> </button>
+					<input className="inputName" type="text" name="FirstName" value="What is your name?" />
+					<div><a className="continueButton btn" onClick={this.toggle}>Continue<i className="arrowIcon material-icons">arrow_forward</i> </a></div>
 				</div>
-			)
-		} else {
+			);
+		}else if(this.state.nameSet) {
 			return(
-				<div>
-					<h1>Become Prposeful</h1>
-					<button className="arrowButtonDown" onClick={this.toggle}> <FaAngleDown/> </button>
-				</div>
+			<div>
+				<input className="inputName" type="text" name="FirstName" value="Email" />
+			</div>
 			);
 		}
 	}
 
-  render(){
-    return(
-      <div className="LandingBack">
-				<img className="logo" src={logo}/>
-        {this.becomePurposeful()}
+	purposeful_Login = () => {
+		return (
+			<div className="login-div">
+				<h2 className="login-h2"> Login </h2>
+				<ul className="icons-list">
+					<li className="login-icon"><div><a href="https://facebook.com"> <FaFacebook className="facebook-icon"/></a></div></li>
+					<li className="login-icon"><div><a href="https://google.com"> <FaGoogle className="google-icon"/></a></div></li>
+					<li className="login-icon"><div><a href="https://linkedin.com"> <FaLinkedin className="linkedin-icon"/></a></div></li>
+				</ul>
+			</div>
+		);
 
-        <div className="navLinks">
-          <p>
-            <a href="dummy1"> _About_Purposeful_ </a>
-            <a href="dummy2"> _Our_Team_ </a>
-            <a href="dummy3"> _Donate_ </a>
-            <a href="dummy4"> _Privacy_Policy_ </a>
-          </p>
-        </div>
+	}
 
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="LandingBack">
+				<img className="logo" src={logo} />
+				{this.becomePurposeful()}
+				{this.purposeful_Signup()}
+				{this.purposeful_Login()}
+
+				<div className="footer-bar">
+					<div className="navLinks">
+						<p>
+							<a href="dummy1"> _About_Purposeful_ </a>
+							<a href="dummy2"> _Our_Team_ </a>
+							<a href="dummy3"> _Donate_ </a>
+							<a href="dummy4"> _Privacy_Policy_ </a>
+						</p>
+					</div>
+				</div>
+
+			</div>
+		);
+	}
 }
 export default LandingPage;
