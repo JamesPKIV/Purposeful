0b9a7254c8fa252710eee12d5c371dfe1398ac09
JamesPKIV <james@bePurposeful.co>
2017-09-21 11:43:19 -0700

    pg test connection

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
+/*postgres test connection*/
+var pgp = require('pg-promise')(/*options*/);
+var db = pgp('postgres://test:test@localhost:5432/purposeful_test_db');
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Getting goodies from the users route!');
