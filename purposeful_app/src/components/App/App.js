106e6eb399aea9d64350595bc07d944cec2d1f20
digonsan <digonsan@hotmail.com>
2017-09-22 12:42:46 -0700

    adapted footer, SEProfile, and landing to mobile view

--- purposeful_app/src/components/App/App.js
+++ purposeful_app/src/components/App/App.js
@@ -1,20 +1,75 @@
 import React, { Component } from 'react';
 import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
 import Dimensions from 'react-dimensions';
-import {Footer} from 'react-materialize';
 
 import './App.css';
-import logo from './logo.png'
-import NavBar from '../NavBar/NavBar'
-import HomePage from '../HomePage/HomePage'
-import MentorshipPage from '../MentorshipPage/MentorshipPage'
-import ProfilePage from '../ProfilePage/ProfilePage'
-import SignupPage from '../SignupPage/SignupPage'
-import LandingPage from '../LandingPage/LandingPage'
-import SEProfilePage from '../SEProfilePage/SEProfilePage'
-
+import NavBar from '../NavBar/NavBar';
+import HomePage from '../HomePage/HomePage';
+import MentorshipPage from '../MentorshipPage/MentorshipPage';
+import ProfilePage from '../ProfilePage/ProfilePage';
+import SignupPage from '../SignupPage/SignupPage';
+import LandingPage from '../LandingPage/LandingPage';
+import SEProfilePage from '../SEProfilePage/SEProfilePage';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.footer = this.footer.bind(this);
+    this.footerDesktop = this.footerDesktop.bind(this);
+    this.footerMobile = this.footerMobile.bind(this);
+  }
+
+
+  footerDesktop(){
+    return(
+      <footer className="page-footer grey darken-4">
+        <div className = "row fullrow">
+          <ul className="footer-links">
+            <li><a href="dummy1" className="desktop-font">About_Purposeful</a></li>
+            <li><a href="dummy2" className="desktop-font">Our_Team</a></li>
+            <li><a href="dummy3" className="desktop-font">Donate</a></li>
+            <li><a href="dummy4" className="desktop-font">Privacy_Policy</a></li>
+          </ul>
+        </div>
+      </footer>
+    );
+  }
+
+  footerMobile(){
+    return(
+      <footer className="page-footer grey darken-4">
+        <div className = "row fullrow">
+          <div className = "col s3 left-align">
+            <a href="dummy1" className="mobile-font">About_Purposeful</a>
+          </div>
+          <div className="col s3 right-align">
+            <a href="dummy2" className="mobile-font">Our_Team</a>
+          </div>
+          <div className= "col s3">
+            <a href="dummy3" className="mobile-font">Donate</a>
+          </div>
+          <div className="col s3">
+            <a href="dummy4" className="mobile-font">Privacy_Policy</a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+
+  footer(props){
+    if (this.props.containerWidth >= 700) {
+      return(
+        this.footerDesktop()
+      );
+    } else {
+      return (
+        this.footerMobile()
+      );
+    }
+  }
+
+
+
   render() {
     return (
       <Router>
@@ -28,7 +83,7 @@ class App extends Component {
             */}
           </header>
 
-          <main className="mainContainer valign-wrapper"> {/*used to be page-content*/}
+          <main className="valign-wrapper"> {/*used to be page-content*/}
               <Route exact path="/" render={() => <Redirect to="/landing" />} />
               <Route path="/home" render={() => <HomePage />} />
               <Route path="/mentorship" render={() => <MentorshipPage />} />
@@ -38,24 +93,7 @@ class App extends Component {
               <Route path="/landing" render={() => <LandingPage />} />
               <Route path="/SEprofile" render={() => <SEProfilePage />} />
           </main>
-
-          <footer className="page-footer grey darken-4">
-            <div className = "row fullrow">
-              <div className = "col s2 push-s4">
-                <a href="dummy1">About_Purposeful</a>
-              </div>
-              <div className="col s2 push-s4">
-                <a href="dummy2">Our_Team</a>
-              </div>
-              <div className= "col s2 push-s4">
-                <a href="dummy3">Donate</a>
-              </div>
-              <div className="col s2 push-s4">
-                <a href="dummy4">Privacy_Policy</a>
-              </div>
-            </div>
-          </footer>
-
+          {this.footer()}
         </div>
       </Router>
     );
