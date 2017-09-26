b9c8a66fdf50089d900a74f34d1263ce94bb5fc0
JamesPKIV <james@bePurposeful.co>
2017-09-25 18:40:25 -0700

    switch DB access from pg-promise to sequelize. queries broken

--- models/pg_database.js
+++ models/pg_database.js
@@ -1,8 +1,23 @@
+const Sequelize = require("sequelize");
 
-/*postgres test connection*/
-const pgp = require('pg-promise')(/*options*/);
-const connectionString = process.env.DATABASE_URL || 
-	"postgres://test:test@localhost:5432/purposeful_test_db";
-const db = pgp(connectionString);
 
-module.exports = {db, pgp};
+/* thhe first argument is the database name ("purposeful_test_db")
+* the second arg is your postgres username
+* the third arg is your postgres password
+*/
+const db = new Sequelize('database', 'username', 'password', {
+  	host: "localhost",
+  	port: "5432",
+  	dialect: "postgres",
+});
+
+
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+module.exports = {db, Sequelize};
