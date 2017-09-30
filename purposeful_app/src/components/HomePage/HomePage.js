2a07ffecf35ccdd2f07c224fd576e99ad9c36055
ogparra <ogparra.15@gmail.com>
2017-09-30 11:22:17 -0700

    Adding current changes to git

--- purposeful_app/src/components/HomePage/HomePage.js
+++ purposeful_app/src/components/HomePage/HomePage.js
@@ -1,40 +1,32 @@
 import React, { Component } from 'react';
 import './HomePage.css';
 import ActivityFeed from '../ActivityFeed/ActivityFeed'
+import NavBar from '../NavBar/NavBar'
 
 class HomePage extends Component {
 
-	constructor (props) {
+	constructor(props) {
 		super(props);
 		this.state = {
-			isLoggedIn : true
+			isLoggedIn: true
 		};
 	}
 
 
-	render () {
+	render() {
 
 		/* conditionally render form content depending on whether youve signed up or not */
 		return (
+
 			<div id="home-content" className="row">
-				{
-					this.state.isLoggedIn ?
-			        <p> you are logged in, and this is your home page. </p>
-			        : <p> you are NOT logged in, and this is your home page. </p>
-			    }
-			    <div className="activity-feeds col l10 push-l1">
-				    {
-				    	this.state.isLoggedIn  &&
-				        <ActivityFeed title="Activity in Your Network" linkTo="/home" />
-	                }
-
-                	<ActivityFeed title="Mentors" linkTo="/mentorship" />
-                	<ActivityFeed title="Mentees" linkTo="/mentorship" />
-                	<ActivityFeed title="Collaborations" linkTo="/home" />
-            	</div>
-
+					<NavBar />
+				<div className="activity-feeds col l10 push-l1">
+					<ActivityFeed title="Activity in Your Network" linkTo="/home" />
+					<ActivityFeed title="Mentorship" linkTo="/mentorship" />
+					<ActivityFeed title="Mentorship" linkTo="/mentorship" />
+				</div>
 			</div>
-        );
+		);
 	}
 }
 
