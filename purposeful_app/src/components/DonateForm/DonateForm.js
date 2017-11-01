0db9a7ebd10b531c2a71a744f38fa0762a530560
ogparra <ogparra.15@gmail.com>
2017-10-31 22:40:48 -0700

    Furthered Donate Page

--- purposeful_app/src/components/DonateForm/DonateForm.js
+++ purposeful_app/src/components/DonateForm/DonateForm.js
@@ -3,190 +3,211 @@ import { Link } from 'react-router-dom';
 import './DonateForm.css';
 import NavBar from '../NavBar/NavBar';
 import working from "../App/still_working.png";
-import teamwork from './teamwork.jpg';
-
-
-const info = ["One time"];
+import interactionsImg from '../AboutPurposeful/interactions.jpg';
+import FaArrowLeft from 'react-icons/lib/fa/arrow-left';
+import FaArrowRight from 'react-icons/lib/fa/arrow-right';
+import FaDollar from 'react-icons/lib/fa/dollar';
 
 class DonateForm extends Component {
-
-	constructor() {
-		super();
-		/* Need to bind this keyword to function before */ 
-		this.proceed = this.proceed.bind(this);
-		this.setInfo = this.setInfo.bind(this);
-		this.setCheck = this.setCheck.bind(this);
+	constructor(props) {
+		super(props);
 		this.state = {
-			content: <Amount proceed={this.proceed} setInfo={this.setInfo} setCheck={this.setCheck} setCheck={this.setCheck} />
-
+			amount: "25.00",
+			selected: "One time",
+			fname: "",
+			lname: "",
+			address: "",
+			zip: "",
+			sstate: "",
+			creditcardholder: "",
+			creditNum: "",
+			expDate: "",
+			securityCode: "",
+			content: "amount"
 		};
+		this.proceed = this.proceed.bind(this);
+		this.setAmount = this.setAmount.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
-	setInfo(idx,  value){
-		let i = 0;
-		while(i < idx + 1 ){
-			if(i == idx){
-				info[i] = value;
-			}
-			i++;
-		}
-		console.log(info[0]);
-		console.log(info[1]);
+	// change state content value
+	proceed(value){ 
+		this.setState({
+			content: value
+		});
 	}
 
-	proceed (value) {
+	// set dontation amount
+	setAmount(ev) {
+		this.setState({
+			amount: ev.target.value
+		});
+		// change style of button for the one selected
+	}
+
+	// handle change for inputs
+	handleChange(ev){
+		this.setState({
+			[ev.target.name]: ev.target.value
+		});
+		console.log(this.state[ev.target.name]);
+	}
+
+	// set content to show
+	setContent(value) {
 		switch (value) {
 			case 'amount':
-				this.setState({
-					content: <Amount proceed={this.proceed} setInfo={this.setInfo} /> 
-				})
-				break;
+				return this.amountContent();
 			case 'details':
-				this.setState({
-					content: <Details proceed={this.proceed} />
-				});
-				break;
+				return this.detailsContent();
 			case 'payment':
-				this.setState({
-					content: <Payment proceed={this.proceed}/>
-				});
-				break;
+				return this.paymentContent();
 			case 'finalize':
-				this.setState({
-					content: <Finalize proceed={this.proceed}/>
-				});
-				break;
+				return this.finalizeContent();
 		}
 	}
 
-	setCheck(e){
-		console.log(e.target.value);
+
+	// donation amount
+	amountContent() {
+		return (
+			<div> <span className="component-title"> Your Support </span>
+				<div className="row">
+					<div className="col s3 m3 l3">
+						<button className="btn amounts" value="25.00" onClick={(e) => this.setAmount(e)}> $25 </button>
+					</div>
+					<div className="col s3 m3 l3">
+						<button className="btn amounts" value="50.00" onClick={(e) => this.setAmount(e)}> $50 </button>
+					</div>
+					<div className="col s3 m3 l3">
+						<button className="btn amounts" value="100.00" onClick={(e) => this.setAmount(e)}> $100 </button>
+					</div>
+					<div className="col s3 m3 l3">
+						<button className="btn amounts" > Other</button>
+					</div>
+				</div>
+				<div className="row" >
+					<form onSubmit={() => this.proceed('details')}>
+						<div className="row">
+							<div className="col s4 m4 l4 offset-s8">
+								<label className="label-radio"> One Time
+									<input type="radio" value="One time" name="selected" checked={this.state.selected === "One time"} onChange={(e) => this.handleChange(e)} />
+								</label>
+							</div>
+							<div className="col s4 m4 l4 offset-s4">
+								<label className="label-radio"> Monthly Recurring
+									<input type="radio" value="Recurring" name="selected" checked={this.state.selected === "Recurring"} onChange={(e) => this.handleChange(e)} />
+								</label>
+							</div>
+						</div>
+						<div className="row">
+							<div className="amount col s6 m6 l6">
+								<FaDollar className="fa-dollar" />
+								<input className="amt-input" value={this.state.amount} />
+							</div>
+							<button className="btn" type="submit" > Continue <FaArrowRight /></button>
+						</div>
+					</form>
+				</div>
+			</div>
+		);
 	}
 
+	// person details
+	detailsContent(){
+		return (
+			<div> <span className="component-title">Details</span>
+				<div className="row">
+					<div className="col s4 m4 l4">
+						<label className="input-label" >First name </label>
+						<input type="text" className="input-style" name="fname" value={this.state.fname} onChange={(e) => this.handleChange(e)}/>
+					</div>
+					<div className="col s4 m4 l4">
+						<label className="input-label">Last name</label>
+						<input type="text" className="input-style" name="lname" value={this.state.lname} onChange={(e) => this.handleChange(e)}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col s4 m4 l4">
+						<label className="input-label">Address </label>
+						<input type="text" className="input-style" name="address" value={this.state.address} onChange={(e) => this.handleChange(e)} />
+					</div>
+					<div className="col s4 m4 l4">
+						<label className="input-label">Zip Code</label>
+						<input type="text" className="input-style" name="zip" value={this.state.zip} onChange={(e) => this.handleChange(e)} />
+					</div>
+					<div className="col s4 m4 l4">
+						<label className="input-label">State </label>
+						<input type="text" className="input-style" name="sstate" value={this.state.sstate} onChange={(e) => this.handleChange(e)}/>
+					</div>
+				</div>
+				<button className="btn back-btn" onClick={() => this.proceed('amount')}> <FaArrowLeft /> Back</button>
+				<button className="btn continue-btn" onClick={() => this.proceed('payment')}>Continue <FaArrowRight /> </button>
+			</div>
+		);
+	}
 
-	Desktop() {
+	// payment details
+	paymentContent(){
+		return (
+			<div> <span className="component-title"> Credit Card Info </span>
+				<div className="row">
+					<div className="col s4 m4 l4">
+						<label className="input-label"> Cardholder Name </label>
+						<input type="text" className="input-style" name="creditcardholder" value={this.state.creditcardholder} onChange={(e) => this.handleChange(e)} />
+					</div>
+					<div className="col s4 m4 l4">
+						<label className="input-label"> Credit/Debit Card Number </label>
+						<input type="text" className="input-style" name="creditNum" value={this.state.creditNum} onChange={(e) => this.handleChange(e)}/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col s3 m3 l3">
+						<label className="input-label"> Expiration Date </label>
+						<input type="date" className="input-style" name="expDate" value={this.state.expDate} onChange={(e) => this.handleChange(e)}/>
+					</div>
+					<div className="col s3 m3 l3">
+						<label className="input-label"> Security Code </label>
+						<input type="text" className="input-style" name="securityCode" value={this.state.securityCode} onChange={(e) => this.handleChange(e)}/>
+					</div>
+				</div>
+				<button className="btn back-btn" onClick={() => this.proceed('details')}> <FaArrowLeft /> Back</button>
+				<button className="btn continue-btn" onClick={() => this.proceed('finalize')}> Continue <FaArrowRight /> </button>
+			</div>
+		);
+	}
+
+	// finalize details
+	finalizeContent() {
+		return (
+			<div>
+				This will be the final component before donation is placed
+				<button className="btn back-btn" onClick={() => this.proceed('payment')}> <FaArrowLeft /> Back</button>
+			</div>
+		);
+	}
+
+	render() {
 		return (
 			<div className="donate-container">
 				<NavBar />
 				<div className="container">
 					<div className="row valign-wrapper ">
-						<div className="col s12">
-							<div className="col s6">
-
+						<div className="col s12 m12 l12">
+							<div className="col s6 m6 l6">
+								<img id="interactions-img" src={interactionsImg} />
+								<div id="donation-desc">
+									Your donation will help continue to support the development and maintence of the website.
+								</div>
 							</div>
-							<div className="col s6">
-							{this.state.content}
+							<div className="col s6 m6 l6">
+								{this.setContent(this.state.content)}
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		);
-	}
-	Mobile() {
-		return (
-			<span>
-				<NavBar />
-				<div>
-					<img src={working} alt="working" width="100vw" className="row center" />
-					<p className="row"> Thanks for your support, this option will be open soon!</p>
-					<Link to="/landing" className="center btn light-green row">Back to Landing Page </Link>
-				</div>
-			</span>
-		);
-	}
-
-	render() {
-		if (window.innerWidth > 700) {
-			return (
-				this.Desktop()
-			);
-		} else {
-			return (
-				this.Mobile()
-			);
-		}
-	}
-}
-
-class Amount extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			selectedOp: "one-time"
-		};
-		this.handleOptionChange = this.handleOptionChange.bind(this);
-	}
-	
-	handleOptionChange(e){
-		alert("Function called");
-		this.setState({
-			selectedOP: e.target.value
-		})
-	}
-
-	render() {
-		return (
-			<div>
-			  <div className="row">
-					<div className="col s4">
-						<button className="btn amounts" onClick={() => this.props.setInfo(1, 25)}> $25 </button>
-					</div>
-					<div className="col s4">
-						<button className="btn amounts" onClick={() => this.props.setInfo(1, 50)}> $50 </button>
-					</div>
-					<div className="col s4">
-						<button className="btn amounts" onClick={() => this.props.setInfo(1, 100)}> $100 </button>
-					</div>
-				</div>
-				<div className="row" >
-					<div className="col s4">
-						<input type="radio" id="one-time" value="one-time" checked={this.state.selectedOp === "one-time"}  onChange={this.handleOptionChange} />
-						<label labelFor="one-time"> One time </label>
-					</div>
-					<div className="col s4">
-						<input type="radio" id="recurring" value="recurring" checked={this.state.selectedOp === "recurring"}  onChange={this.handleOptionChange} />
-						<label labelFor="recurring"> Recurring </label>
-					</div>
-				</div>
-				<div> 
-					<input className="other-amount" placeholder="Other Amount" /> 
-				</div> 
-				<button className="btn" onClick={() => this.props.proceed('details')}> Next</button>
-			</div>
-		);
-	}
-}
-
-class Details extends Component {
-	render() {
-		return (
-			<div> Details
-				<button className="btn" onClick={() => this.props.proceed('payment')}> Next </button>
-				<button className="btn" onClick={() => this.props.proceed('amount')}> Back</button>
-			</div>
-		)
-	}
-}
-
-class Payment extends Component {
-	render(){
-		return(
-			<div> Payment
-				<button className="btn" onClick={() => this.props.proceed('finalize')}> Next </button>
-				<button className="btn" onClick={() => this.props.proceed('details')}> Back</button>
-			</div>
-		)
-	}
-}
-
-class Finalize extends Component{
-	render(){
-		return(
-			<div>
-				This will be the final component before donation is placed
-			</div>
-		)
 	}
 }
 
